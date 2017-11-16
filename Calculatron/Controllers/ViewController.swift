0fd5693c593aed295247b4eb2b7b1df66b3b3059
Christopher Smith <Chris.Software.Developer@gmail.com>
2017-11-16 15:38:19 -0800

    Added logic for numbers and operators

--- Calculatron/Controllers/ViewController.swift
+++ Calculatron/Controllers/ViewController.swift
@@ -10,5 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    var value1: Double
+    var value2: Double
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    // MARK: - IBActions
+    
+    @IBAction func numberPushed(_ sender: UIButton) {
+        
+        let number = sender.tag
+        
+        resultsLabel.text = resultsLabel.text! + String(number)
+        }
+    
+    @IBAction func operatorPushed(_ sender: UIButton) {
+        
+        self.operate(sender)
+        }
+    
+    // MARK: - Convenience Methods
+    
+    func operate(_ sender: UIButton) {
+        
+        if sender.currentTitle == "+" {
+            self.resultsLabel.text = String(value1 + value2)
+        }
+            
+        else if sender.currentTitle == "-" {
+            self.resultsLabel.text = String (value1 - value2)
+        }
+            
+        else if sender.currentTitle == "*" {
+            self.resultsLabel.text = String (value1 * value2)
+        }
+            
+        else if sender.currentTitle == "÷" {
+            self.resultsLabel.text = String (value1 / value2)
+        }
+            
+        else {
+            return
+        }
+    }
 }
-
