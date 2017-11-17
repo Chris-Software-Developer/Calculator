db564730d89a53436650e7126daf59a738e8a960
David Seitz Jr <davidsights@me.com>
2017-11-16 16:35:34 -0800

    Added addition functionality - Some light refactoring. - Implemented addition functionality. - Added clearing functionality. - Warning: Equals button doesn't work yet. But you can add things over and over again with just the add button. - Added gitignore to ignore xcuserdata.

--- Calculatron/Controllers/ViewController.swift
+++ Calculatron/Controllers/ViewController.swift
@@ -11,9 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     // MARK: Properties
-    
-    var value1: Double
-    var value2: Double
+
+    /// Stores the value to be operated on.
+    var value1: Double?
+
+    /// The last operator that was selected.
+    var currentlySelectedOperator: String?
+
+    var operatorJustPressed = false
     
     // MARK: IBOutlets
     
@@ -21,40 +26,93 @@ class ViewController: UIViewController {
     
     // MARK: - IBActions
     
-    @IBAction func numberPushed(_ sender: UIButton) {
-        
-        let number = sender.tag
-        
-        resultsLabel.text = resultsLabel.text! + String(number)
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+
+        if self.operatorJustPressed {
+            self.resultsLabel.text = ""
+            self.operatorJustPressed = false
         }
+
+        if let numberString = sender.titleLabel?.text {
+            self.resultsLabel.text = resultsLabel.text! + numberString
+        }
+    }
     
     @IBAction func operatorPushed(_ sender: UIButton) {
-        
-        self.operate(sender)
+
+        if let operatorString = sender.titleLabel?.text {
+            self.handleOperation(operatorString)
         }
-    
+    }
+
+    @IBAction func clearButtonPressed(_ sender: Any) {
+        self.value1 = nil
+        self.resultsLabel.text = ""
+        self.currentlySelectedOperator = nil
+    }
+
     // MARK: - Convenience Methods
-    
-    func operate(_ sender: UIButton) {
-        
-        if sender.currentTitle == "+" {
-            self.resultsLabel.text = String(value1 + value2)
+
+    func handleOperation(_ operation: String) {
+
+        self.operatorJustPressed = true
+
+        // Get the basic values you need.
+
+        guard
+            let labelText = self.resultsLabel.text,
+            let value = Double(labelText) else {
+                fatalError("Something went wrong while trying to retrieve these values. See if any of these aren't beeing set correctly.")
         }
-            
-        else if sender.currentTitle == "-" {
-            self.resultsLabel.text = String (value1 - value2)
-        }
-            
-        else if sender.currentTitle == "*" {
-            self.resultsLabel.text = String (value1 * value2)
-        }
-            
-        else if sender.currentTitle == "÷" {
-            self.resultsLabel.text = String (value1 / value2)
-        }
-            
-        else {
+
+        // Check if it's time to operate, or time to store.
+
+        if self.currentlySelectedOperator == nil && self.value1 == nil {
+
+            // This is the first operation selected. Store the relevant values.
+
+            self.value1 = value
+            self.currentlySelectedOperator = operation
+
+            // Clear the label for the next value.
+
+            self.resultsLabel.text = ""
+
+            // And get out of here so we can get the second value.
+
             return
+
+        } else {
+
+            guard
+                let value1 = self.value1,
+                let value2String = self.resultsLabel.text,
+                let value2 = Double(value2String) else {
+                    fatalError("Couldn't get values. Check to see if these are being accessed correctly.")
+            }
+
+            //  value1 is available, time to operate
+
+            switch operation {
+
+            case "+":
+                let result = value1 + value2
+                self.value1 = result
+                self.resultsLabel.text = "\(result)"
+
+            case "-":
+                break // This is only needed because nothing is happening here.
+
+
+            case "/":
+                break // This is only needed because nothing is happening here.
+
+            case "*":
+                break // This is only needed because nothing is happening here.
+
+            default:
+                break
+            }
         }
     }
 }
